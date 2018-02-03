96e74f7910fa528da29c178596c77b633ee6d04d
Bootcamp <njackson422@gmail.com>
2018-02-02 21:02:59 -0500

    Adds starter files

---
+++ success-dashboard/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleNavbar} from './styled_components/Navbar';
+
+export const Navbar = () => {
+	return (
+		<StyleNavbar>
+			<li>Home</li>
+			<li>Notes</li>
+			<li>Logout</li>
+		</StyleNavbar>
+		)
+};
---
+++ success-dashboard/src/components/styled_components/Navbar.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyleNavbar = styled.ul`
+
+	list-style: none;
+	width: 100vw;
+	height: 60px;
+    background: red;
+    display: flex;
+    align-items: center;
+
+    li {
+	  	font-size: 18px;
+	    padding: 0 10px;
+    }
+` 

04df282f554dbe6d87535635077677f16417f198
Bootcamp <njackson422@gmail.com>
2018-02-04 19:35:21 -0500

    Adds styled-components subfolders

---
+++ success-dashboard/src/components/MainDash/DashStyles/CreateNote.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyleCreateNote = styled.form`
+
+	width: 50vw;
+	height: 100px;
+    background: white;
+    display: flex;
+    align-content: center;
+
+` 

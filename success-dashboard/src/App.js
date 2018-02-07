4ad77d5bbcd36ff3f92ecfffddd5354b2195c752
Bootcamp <njackson422@gmail.com>
2018-02-06 19:43:20 -0500

    implements new routing to page containers

--- success-dashboard/src/App.js
+++ success-dashboard/src/App.js
@@ -10,21 +10,35 @@ import {TechArticles} from './components/MainDash/TechArticles.js';
 import {MeetupFeed} from './components/MainDash/MeetupFeed.js';
 import {ToDoList} from './components/MainDash/ToDoList.js';
 
-export const App = () => {
-  return (
-    <MuiThemeProvider>
-    	<div>
-    		<Navbar />
-    		<Trivia />
-    		<GoodVibesNews />
-    		<TechArticles />
-    		<MeetupFeed />
-    		<ToDoList />
-    		<CreateNote />
-    	</div>
-    </MuiThemeProvider>
 
-  	);
+//******Still need to create these container components named below***********
+export const App = () =>
+  <Router>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/notes" component={NoteManager} />
+    </div>
+  </Router>;
 
-};
+
+// export const App = () => {
+//   return (
+//     <MuiThemeProvider>
+//     	<div>
+//     		<Navbar />
+//     		<Trivia />
+//     		<GoodVibesNews />
+//     		<TechArticles />
+//     		<MeetupFeed />
+//     		<ToDoList />
+//     		<CreateNote />
+//     	</div>
+//     </MuiThemeProvider>
+
+//   	);
+
+// };
+
+
 

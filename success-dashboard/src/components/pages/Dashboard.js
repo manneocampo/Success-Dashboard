7aef887ed43e538b379dd1e316693dd1a3d0fdcc
Bootcamp <njackson422@gmail.com>
2018-02-09 16:07:07 -0500

    fixes bug from last deploy. cleans up App.js and adds a 404 route.

--- success-dashboard/src/components/pages/Dashboard.js
+++ success-dashboard/src/components/pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Trivia from '../MainDash/Trivia';
 import { API } from '../../utils/API';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -43,6 +44,33 @@ export default class Dashboard extends React.Component {
           </MuiThemeProvider>
         </div>
 
+
+// import { Route } from 'react-router-dom';
+// import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// // import './App.css';
+// import Navbar from '../Navbar.js';
+// import {Trivia} from '../MainDash/Trivia.js';
+// import {GoodVibesNews} from '../MainDash/GoodVibesNews.js';
+// import {TechArticles} from '../MainDash/TechArticles.js';
+// import {MeetupFeed} from '../MainDash/MeetupFeed.js';
+// import {ToDoList} from '../MainDash/ToDoList.js';
+// import {CreateNote} from '../MainDash/CreateNote.js';
+//
+//  const Dashboard = () => {
+//     return (
+//   		<MuiThemeProvider>
+// 	    	<div>
+// 	    		<Trivia />
+// 	    		<GoodVibesNews />
+// 	    		<TechArticles />
+// 	    		<MeetupFeed />
+// 	    		<ToDoList />
+// 	    		<CreateNote />
+// 	    	</div>
+//     	</MuiThemeProvider>
   	)
   }
 };
+
+export default Dashboard;
